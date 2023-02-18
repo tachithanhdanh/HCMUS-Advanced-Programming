012fd660a32f7793710143e7aea52e102cf26754
tachithanhdanh-desktop <22120049@student.hcmus.edu.vn>
2023-02-18 19:03:10 +0700

    first ever commit

---
+++ test_folder/blah.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using std::cout;
+
+int main(void) {
+    int a = 5;
+    cout << "Hello World!\n";
+    cout << "There is something that has changed. I can feel it.\n";
+    return 0;
+}
