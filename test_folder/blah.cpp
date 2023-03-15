2a4044f34136bf6dd540cc3cf886a8b7e00792c0
tachithanhdanh-laptop <22120049@student.hcmus.edu.vn>
2023-03-15 14:43:31 +0700

    thay doi cau truc thu muc

--- test_folder/blah.cpp
+++ test_folder/blah.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 
+using pint = int *;
+
 int main()
 {
-    int* ptr{ new int }; // dynamically allocate an integer
+    pint ptr{ new int }; // dynamically allocate an integer
     *ptr = 7; // put a value in that memory location
     std::cout << *ptr;
     std::cout << "Hello World!\n";
