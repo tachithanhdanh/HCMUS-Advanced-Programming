3aaf8957d1a3b3a6307218b139d8e2bf18f63eec
tachithanhdanh-laptop <22120049@student.hcmus.edu.vn>
2023-04-13 12:45:52 +0700

    Add forward declarations for KnightTour.hpp

--- Lectures/Week07.AdvancedRecursion/Knight-Tour/KnightTour.hpp
+++ Lectures/Week07.AdvancedRecursion/Knight-Tour/KnightTour.hpp
@@ -1,4 +1,7 @@
 #ifndef WEEK07_KNIGHT_TOUR_HPP_
 #define WEEK07_KNIGHT_TOUR_HPP_
 
+bool isValid(int x, int y, int n, int **position);
+void knightTour(int k, int n, int x, int y, int **position, const int dx[8], const int dy[8]);
+
 #endif
