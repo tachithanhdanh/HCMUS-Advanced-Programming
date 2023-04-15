26dcb5f252942a3825ab148f7477f92abf59ec1d
tachithanhdanh-desktop <22120049@student.hcmus.edu.vn>
2023-04-15 18:50:58 +0700

    minor change to mitigate warning in VS

--- Practice/Lab05.SinglyLinkedList/22120049.cpp
+++ Practice/Lab05.SinglyLinkedList/22120049.cpp
@@ -9,14 +9,14 @@ private:
 
 public:
     Fraction(int num = 0, int den = 1);
-    Fraction(const Fraction& frac);
-    Fraction& operator=(const Fraction& frac);
+    Fraction(const Fraction &frac);
+    Fraction &operator=(const Fraction &frac);
     ~Fraction();
     void reduce();
-    friend std::istream& operator>>(std::istream& in, Fraction& frac);
-    friend std::ostream& operator<<(std::ostream& out, const Fraction& frac);
-    friend bool operator==(const Fraction& f1, const Fraction& f2);
-    friend bool operator!=(const Fraction& f1, const Fraction& f2);
+    friend std::istream &operator>>(std::istream &in, Fraction &frac);
+    friend std::ostream &operator<<(std::ostream &out, const Fraction &frac);
+    friend bool operator==(const Fraction &f1, const Fraction &f2);
+    friend bool operator!=(const Fraction &f1, const Fraction &f2);
 };
 
 const Fraction nullFrac(0, 0);
@@ -24,60 +24,60 @@ const Fraction nullFrac(0, 0);
 struct Node {
 private:
     Fraction m_data;
-    Node* m_pNext;
+    Node *m_pNext;
 
 public:
     Node();
-    Node(Fraction data, Node* pNext = nullptr);
+    Node(Fraction data, Node *pNext = nullptr);
     ~Node();
-    Node(const Node& node);
-    Node& operator=(const Node& node);
-    Node* nextNode() const;
-    const Fraction& getData() const;
-    void setNextNode(Node* pNext);
-    friend std::ostream& operator<<(std::ostream& out, const Node& node);
+    Node(const Node &node);
+    Node &operator=(const Node &node);
+    Node *nextNode() const;
+    const Fraction &getData() const;
+    void setNextNode(Node *pNext);
+    friend std::ostream &operator<<(std::ostream &out, const Node &node);
     // friend std::istream& operator>>(std::istream& in, Node& node);
 };
 
 struct SLL {
 private:
-    Node* m_pHead = nullptr;
+    Node *m_pHead = nullptr;
 
 public:
     SLL();
     ~SLL();
-    SLL(const SLL& list);
-    SLL& operator=(const SLL& list);
-    void deepCopy(const SLL& source);
+    SLL(const SLL &list);
+    SLL &operator=(const SLL &list);
+    void deepCopy(const SLL &source);
     bool isEmpty() const;
-    void setHead(Node* pNode);
-    Node* getHead() const;
-    Node* getKthElement(int k) const;
-    Node* getTail() const;
-    void insertAtBeginning(const Fraction& frac);
-    void insertAtEnding(const Fraction& frac);
-    void insertAtKthElement(const Fraction& frac, int k);
-    void insertAfterElement(Node* pNode, const Fraction& frac);
+    void setHead(Node *pNode);
+    Node *getHead() const;
+    Node *getKthElement(int k) const;
+    Node *getTail() const;
+    void insertAtBeginning(const Fraction &frac);
+    void insertAtEnding(const Fraction &frac);
+    void insertAtKthElement(const Fraction &frac, int k);
+    void insertAfterElement(Node *pNode, const Fraction &frac);
     void deleteAtBeginning();
     void deleteAtEnding();
-    void deleteANode(const Fraction& _free_locale);
+    void deleteANode(const Fraction &_free_locale);
     void deleteKthNode(int k);
     void deleteAllNode();
     void inputSLL();
     void displaySLL() const;
     int countNode() const;
-    friend std::ostream& operator<<(std::ostream& out, const SLL& list);
+    friend std::ostream &operator<<(std::ostream &out, const SLL &list);
     // friend std::istream& operator>>(std::istream& in, SLL& list);
 };
 
 void solve();
 int gcd(int a, int b);
-void exercise1(SLL*& list);
-void exercise2(SLL*& list);
-void exercise3(SLL*& list);
-void exercise4(SLL*& list);
-void exercise5(SLL*& list);
-void exercise6(SLL*& list);
+void exercise1(SLL *&list);
+void exercise2(SLL *&list);
+void exercise3(SLL *&list);
+void exercise4(SLL *&list);
+void exercise5(SLL *&list);
+void exercise6(SLL *&list);
 
 int main(void) {
     solve();
@@ -88,7 +88,7 @@ void solve() {
     std::cout << "Da em chao thay.\n";
     std::cout << "Day la bai lam cua Thanh Danh - MSSV 22120049.\n";
 
-    SLL* list = new SLL();
+    SLL *list = new SLL();
 
     exercise1(list);
     exercise2(list);
@@ -104,20 +104,20 @@ void solve() {
     return;
 }
 
-void exercise1(SLL*& list) {
+void exercise1(SLL *&list) {
     std::cout << "Yeu cau 1: Xay dung danh sach cac phan so.\n";
     list->inputSLL();
     return;
 }
 
-void exercise2(SLL*& list) {
+void exercise2(SLL *&list) {
     std::cout << "Yeu cau 2: Xuat danh sach cac phan so sau khi da xay dung.\n";
     std::cout << "Danh sach phan so sau khi da xay dung.\n";
     list->displaySLL();
     return;
 }
 
-void exercise3(SLL*& list) {
+void exercise3(SLL *&list) {
     std::cout << "Yeu cau 3: Them mot phan so vao cuoi danh sach.\n";
     Fraction frac;
     std::cout << "Moi thay nhap phan so (dinh dang a/b, vi du 1/2): ";
@@ -128,7 +128,7 @@ void exercise3(SLL*& list) {
     return;
 }
 
-void exercise4(SLL*& list) {
+void exercise4(SLL *&list) {
     using std::cout;
     using std::cin;
     cout << "Yeu cau 4: Chen mot phan so tai mot vi tri xac dinh trong danh sach.\n";
@@ -139,13 +139,13 @@ void exercise4(SLL*& list) {
     cout << "Moi thay nhap vi tri can chen trong danh sach: ";
     cin >> k;
     list->insertAtKthElement(frac, k);
-    cout << "Danh sach phan so sau khi da them phan so moi tai vi tri " 
-         << k << " trong danh sach.\n";
+    cout << "Danh sach phan so sau khi da them phan so moi tai vi tri "
+        << k << " trong danh sach.\n";
     list->displaySLL();
     return;
 }
 
-void exercise5(SLL*& list) {
+void exercise5(SLL *&list) {
     using std::cout;
     using std::cin;
     cout << "Yeu cau 5: Xoa mot phan so tai mot vi tri xac dinh trong danh sach.\n";
@@ -153,13 +153,13 @@ void exercise5(SLL*& list) {
     int k;
     cin >> k;
     list->deleteKthNode(k);
-    cout << "Danh sach phan so sau khi da xoa phan so tai vi tri " 
-         << k << " trong danh sach.\n";
+    cout << "Danh sach phan so sau khi da xoa phan so tai vi tri "
+        << k << " trong danh sach.\n";
     list->displaySLL();
     return;
 }
 
-void exercise6(SLL*& list) {
+void exercise6(SLL *&list) {
     using std::cout;
     using std::cin;
     cout << "Yeu cau 6: Huy toan bo danh sach phan so.\n";
@@ -180,13 +180,13 @@ int gcd(int a, int b) {
     return std::abs(a);
 }
 
-Fraction::Fraction(int num, int den): m_numerator(num), m_denominator(den) {
+Fraction::Fraction(int num, int den) : m_numerator(num), m_denominator(den) {
     this->reduce();
 }
 
-Fraction::Fraction(const Fraction& frac): Fraction(frac.m_numerator, frac.m_denominator) {}
+Fraction::Fraction(const Fraction &frac) : Fraction(frac.m_numerator, frac.m_denominator) {}
 
-Fraction& Fraction::operator=(const Fraction& frac) {
+Fraction &Fraction::operator=(const Fraction &frac) {
     // self-assignment check
     if (this == &frac) {
         return *this;
@@ -216,29 +216,29 @@ void Fraction::reduce() {
     return;
 }
 
-std::istream& operator>>(std::istream& in, Fraction& frac) {
+std::istream &operator>>(std::istream &in, Fraction &frac) {
     in >> frac.m_numerator;
     in.ignore(std::numeric_limits<std::streamsize>::max(), '/');
     in >> frac.m_denominator;
     return in;
 }
 
-std::ostream& operator<<(std::ostream& out, const Fraction& frac) {
+std::ostream &operator<<(std::ostream &out, const Fraction &frac) {
     out << frac.m_numerator << '/' << frac.m_denominator;
     return out;
 }
 
-bool operator==(const Fraction& f1, const Fraction& f2) {
+bool operator==(const Fraction &f1, const Fraction &f2) {
     return (f1.m_numerator == f2.m_numerator) && (f1.m_denominator == f2.m_denominator);
 }
 
-bool operator!=(const Fraction& f1, const Fraction& f2) {
+bool operator!=(const Fraction &f1, const Fraction &f2) {
     return !(operator==(f1, f2));
 }
 
-Node::Node(): m_data(0), m_pNext(nullptr) {}
+Node::Node() : m_data(0), m_pNext(nullptr) {}
 
-Node::Node(Fraction data, Node* pNext): m_data(data), m_pNext(pNext) {}
+Node::Node(Fraction data, Node *pNext) : m_data(data), m_pNext(pNext) {}
 
 Node::~Node() {
     std::cout << "Node destructor called for node has data = " << this->getData();
@@ -247,9 +247,9 @@ Node::~Node() {
     // this->m_pNext = nullptr;
 }
 
-Node::Node(const Node& node): m_data(node.m_data), m_pNext(node.m_pNext) {}
+Node::Node(const Node &node) : m_data(node.m_data), m_pNext(node.m_pNext) {}
 
-Node& Node::operator=(const Node& node) {
+Node &Node::operator=(const Node &node) {
     // self-assignment check
     if (this == &node) {
         return *this;
@@ -260,22 +260,22 @@ Node& Node::operator=(const Node& node) {
     return *this;
 }
 
-Node* Node::nextNode() const {
+Node *Node::nextNode() const {
     return this->m_pNext;
 }
 
-const Fraction& Node::getData() const {
+const Fraction &Node::getData() const {
     return this->m_data;
 }
 
-void Node::setNextNode(Node* pNode) {
+void Node::setNextNode(Node *pNode) {
     this->m_pNext = pNode;
     return;
 }
 
-std::ostream& operator<<(std::ostream& out, const Node& node) {
+std::ostream &operator<<(std::ostream &out, const Node &node) {
     out << "Current node: data = " << node.getData();
-    out << ", address = " << &node; 
+    out << ", address = " << &node;
     out << ", pNext = " << node.nextNode() << '\n';
     return out;
 }
@@ -285,18 +285,18 @@ std::ostream& operator<<(std::ostream& out, const Node& node) {
 //     return in;
 // }
 
-SLL::SLL(): m_pHead(nullptr) {}
+SLL::SLL() : m_pHead(nullptr) {}
 
 SLL::~SLL() {
-    std::cout << "Singly Linked List destructor called.\n";
+    std::cout << "Singly Linked List destructor called\n";
     this->deleteAllNode();
 }
 
-SLL::SLL(const SLL& list) {
+SLL::SLL(const SLL &list) {
     this->deepCopy(list);
 }
 
-SLL& SLL::operator=(const SLL& list) {
+SLL &SLL::operator=(const SLL &list) {
     // self-assigment check
     if (this == &list) {
         return *this;
@@ -309,18 +309,18 @@ SLL& SLL::operator=(const SLL& list) {
     return *this;
 }
 
-void SLL::deepCopy(const SLL& list) {
+void SLL::deepCopy(const SLL &list) {
     // this->deleteAllNode();
     this->setHead(nullptr);
-    Node* pNode = list.getHead();
-    Node* cur = nullptr;
+    Node *pNode = list.getHead();
+    Node *cur = nullptr;
     for (; pNode; pNode = pNode->nextNode()) {
         if (this->getHead() == nullptr) {
             this->setHead(new Node(pNode->getData()));
             cur = this->getHead();
         } else {
             cur->setNextNode(new Node(pNode->getData()));
-            cur = cur->nextNode();
+            if (cur) cur = cur->nextNode();
         }
     }
     return;
@@ -330,46 +330,46 @@ bool SLL::isEmpty() const {
     return this->getHead() == nullptr;
 }
 
-void SLL::setHead(Node* pNode) {
+void SLL::setHead(Node *pNode) {
     this->m_pHead = pNode;
     return;
 }
 
-Node* SLL::getHead() const {
+Node *SLL::getHead() const {
     return this->m_pHead;
 }
 
-Node* SLL::getKthElement(int k) const {
-    Node* pNode = this->getHead();
-    for(int i = 0; pNode && i != k; pNode = pNode->nextNode(), ++i);
+Node *SLL::getKthElement(int k) const {
+    Node *pNode = this->getHead();
+    for (int i = 0; pNode && i != k; pNode = pNode->nextNode(), ++i);
     return pNode;
 }
 
-Node* SLL::getTail() const {
+Node *SLL::getTail() const {
     if (isEmpty()) return nullptr;
-    Node* pNode = this->getHead();
+    Node *pNode = this->getHead();
     for (; pNode->nextNode(); pNode = pNode->nextNode());
     return pNode;
 }
 
-void SLL::insertAtBeginning(const Fraction& frac) {
-    Node* new_pHead = new Node(frac, this->getHead());
+void SLL::insertAtBeginning(const Fraction &frac) {
+    Node *new_pHead = new Node(frac, this->getHead());
     this->setHead(new_pHead);
     return;
 }
 
-void SLL::insertAtEnding(const Fraction& frac) {
-    Node* pTail = this->getTail();
+void SLL::insertAtEnding(const Fraction &frac) {
+    Node *pTail = this->getTail();
     if (pTail == nullptr) {
         this->insertAtBeginning(frac);
         return;
-    } 
-    Node* new_pTail = new Node(frac);
+    }
+    Node *new_pTail = new Node(frac);
     pTail->setNextNode(new_pTail);
     return;
 }
 
-void SLL::insertAtKthElement(const Fraction& frac, int k) {
+void SLL::insertAtKthElement(const Fraction &frac, int k) {
     // chia 2 TH
     // TH1: so am hoac vuot qua size cua linked list 
     //      -> khong chen, bao loi
@@ -397,26 +397,26 @@ void SLL::insertAtKthElement(const Fraction& frac, int k) {
     return;
 }
 
-void SLL::insertAfterElement(Node* pNode, const Fraction& frac) {
+void SLL::insertAfterElement(Node *pNode, const Fraction &frac) {
     // only insert if it is not nullptr
     if (pNode) {
-        Node* pNext = pNode->nextNode();
-        Node* new_pNode = new Node(frac, pNext);
+        Node *pNext = pNode->nextNode();
+        Node *new_pNode = new Node(frac, pNext);
         pNode->setNextNode(new_pNode);
     }
     return;
 }
 
 void SLL::deleteAtBeginning() {
-    Node* old_pHead = this->getHead();
-    if(this->getHead()) this->setHead(this->getHead()->nextNode());
+    Node *old_pHead = this->getHead();
+    if (this->getHead()) this->setHead(this->getHead()->nextNode());
     delete old_pHead;
     return;
 }
 
-void SLL::deleteANode(const Fraction& frac) {
-    Node* prev = nullptr;
-    Node* cur = this->getHead();
+void SLL::deleteANode(const Fraction &frac) {
+    Node *prev = nullptr;
+    Node *cur = this->getHead();
     for (; cur; prev = cur, cur = cur->nextNode()) {
         if (cur->getData() == frac) {
             // if there is no prev, it must be a head.
@@ -449,8 +449,8 @@ void SLL::deleteKthNode(int k) {
         this->deleteAtBeginning();
         return;
     }
-    Node* prev = nullptr;
-    Node* cur = this->getHead();
+    Node *prev = nullptr;
+    Node *cur = this->getHead();
 
     // find k-th node
     for (int i = 0; i != k && cur; prev = cur, cur = cur->nextNode(), ++i);
@@ -466,7 +466,7 @@ void SLL::deleteKthNode(int k) {
 }
 
 void SLL::deleteAtEnding() {
-    Node* pNode = this->getHead();
+    Node *pNode = this->getHead();
 
     // if a list is empty then do nothing
     if (pNode == nullptr) {
@@ -488,20 +488,20 @@ void SLL::deleteAtEnding() {
 }
 
 void SLL::deleteAllNode() {
-    std::cout << "SLL::deleteAllNode() function called.\n";
-    while(this->getHead()) {
+    std::cout << "SLL::deleteAllNode function called.\n";
+    while (this->getHead()) {
         this->deleteAtBeginning();
     }
     return;
 }
 
-std::ostream& operator<<(std::ostream& out, const SLL& list) {
+std::ostream &operator<<(std::ostream &out, const SLL &list) {
     if (list.getHead() == nullptr) {
         out << "Empty List!\n";
         return out;
     }
     out << "Singly Linked List (print order from head to tail):\n";
-    for (Node* pNode = list.getHead(); pNode; pNode = pNode->nextNode()) {
+    for (Node *pNode = list.getHead(); pNode; pNode = pNode->nextNode()) {
         out << *pNode;
     }
     return out;
@@ -520,7 +520,7 @@ void SLL::inputSLL() {
     cout << "Vi du: 1/2, 3/4, 4/5, ...\n";
     cout << "De dung viec nhap danh sach phan so, thay vui long nhap 0/0.\n";
     Fraction frac;
-    Node* cur = nullptr;
+    Node *cur = nullptr;
     do {
         cout << "Moi thay nhap phan so (nhap 0/0 de dung): ";
         std::cin >> frac;
@@ -530,7 +530,7 @@ void SLL::inputSLL() {
                 cur = this->getHead();
             } else {
                 cur->setNextNode(new Node(frac));
-                cur = cur->nextNode();
+                if(cur) cur = cur->nextNode();
             }
         }
     } while (frac != nullFrac);
@@ -544,7 +544,7 @@ void SLL::displaySLL() const {
 
 int SLL::countNode() const {
     int count = 0;
-    for (Node* pNode = this->getHead(); pNode; pNode = pNode->nextNode()) {
+    for (Node *pNode = this->getHead(); pNode; pNode = pNode->nextNode()) {
         ++count;
     }
     return count;
